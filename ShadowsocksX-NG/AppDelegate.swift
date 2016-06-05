ae8848d4e837805fe53a8e32f39afb435b1b0670
Charlie Qiu <qbowater@gmail.com>
2016-06-05 21:29:13 +0800

    Initial Commit

---
+++ ShadowsocksX-NG/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ShadowsocksX-NG
+//
+//  Created by 邱宇舟 on 16/6/5.
+//  Copyright © 2016年 qiuyuzhou. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
