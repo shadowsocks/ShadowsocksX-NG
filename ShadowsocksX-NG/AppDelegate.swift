shadowsocks/ShadowsocksX-NG

commit dd20ebda682b9c2ed301c5dc9464debc497e76f1

fix crash problem

ShadowsocksX-NG/AppDelegate.swift
@@ -385,14 +385,14 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSUserNotificationCenterDele
         showRunningModeMenuItem.state = defaults.bool(forKey: "ShowRunningModeOnStatusBar") ? 1 : 0
         
         var serverMenuText = "Servers".localized
-        for v in defaults.array(forKey: "ServerProfiles")! {
-            let profile = v as! [String:Any]
-            if profile["Id"] as! String == defaults.string(forKey: "ActiveServerProfileId")! {
+        let mgr = ServerProfileManager.instance
+        for p in mgr.profiles {
+            if mgr.activeProfileId == p.uuid {
                 var profileName :String
-                if profile["Remark"] as! String != "" {
-                    profileName = profile["Remark"] as! String
+                if !p.remark.isEmpty {
+                    profileName = p.remark
                 } else {
-                    profileName = profile["ServerHost"] as! String
+                    profileName = p.serverHost
                 }
                 serverMenuText = "\(serverMenuText) - \(profileName)"
             }

commit 7ad9dc42a03858883f8f9e3cf7eebe7b1a4a0254

Fix bug caused by pr #97

ShadowsocksX-NG/AppDelegate.swift
@@ -395,6 +395,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSUserNotificationCenterDele
                 } else {
                     profileName = p.serverHost
                 }
+                serverMenuText = "\(serverMenuText) - \(profileName)"
             }
         }
         serversMenuItem.title = serverMenuText