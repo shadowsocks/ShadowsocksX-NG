faf029c7f7ecbf307b2fd77c464bd73710290586
Rainux Luo <rainux@gmail.com>
2017-01-07 06:54:31 +0800

    Parse SS URL with much cleaner URL struct methods

--- ShadowsocksX-NG/Utils.swift
+++ ShadowsocksX-NG/Utils.swift
@@ -8,13 +8,11 @@
 
 import Foundation
 
-
 extension String {
     var localized: String {
         return NSLocalizedString(self, tableName: nil, bundle: Bundle.main, value: "", comment: "")
     }
 }
-
 
 extension Data {
     func sha1() -> String {
@@ -24,4 +22,37 @@ extension Data {
         let hexBytes = digest.map { String(format: "%02hhx", $0) }
         return hexBytes.joined(separator: "")
     }
+}
+
+func ParseSSURL(_ url: URL?) -> [String: Any?]? {
+
+    func padBase64(string: String) -> String {
+        var length = string.characters.count
+        length = 4 - length % 4 + length
+        return string.padding(toLength: length, withPad: "=", startingAt: 0)
+    }
+
+    if url?.host == nil {
+        return nil
+    }
+
+    var plainUrl: URL! = url
+
+    let data = Data(base64Encoded: padBase64(string: url!.host!),
+                    options: Data.Base64DecodingOptions())
+
+    if data != nil {
+        let decoded = String(data: data!, encoding: String.Encoding.utf8)
+        plainUrl = URL(string: "ss://\(decoded!)")
+
+        if plainUrl == nil {
+            return nil
+        }
+    }
+
+    return ["ServerHost": plainUrl.host,
+            "ServerPort": UInt16(plainUrl.port!),
+            "Method": plainUrl.user,
+            "Password": plainUrl.password,
+    ]
 }
--- ShadowsocksX-NGTests/UtilsTests.swift
+++ ShadowsocksX-NGTests/UtilsTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+@testable import ShadowsocksX_NG
 
 class UtilsTests: XCTestCase {
 
