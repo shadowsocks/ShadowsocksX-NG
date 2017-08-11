dc44449046a6fe73c90b60ae94ba7fb97ed4e748
Dig Para <dig@para.pub>
2017-08-11 13:40:58 +0800

    Fix ss_local start issue. The `launchctl load` failed with "Path had bad ownership/permissions". Add chmod 444 command for the plist file.  Remove plist write permission.

--- ShadowsocksX-NG/start_kcptun.sh
+++ ShadowsocksX-NG/start_kcptun.sh
@@ -6,5 +6,6 @@
 #  Created by 邱宇舟 on 2017/1/11.
 #  Copyright © 2017年 qiuyuzhou. All rights reserved.
 
+chmod 444 "$HOME/Library/LaunchAgents/com.qiuyuzhou.shadowsocksX-NG.kcptun.plist"
 launchctl load "$HOME/Library/LaunchAgents/com.qiuyuzhou.shadowsocksX-NG.kcptun.plist"
 launchctl start com.qiuyuzhou.shadowsocksX-NG.kcptun
