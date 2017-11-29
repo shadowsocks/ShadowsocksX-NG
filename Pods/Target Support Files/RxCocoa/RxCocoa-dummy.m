1989c79fd4f180951b9b1f1f479b829e735d7399
ruqli <ruqli@microsoft.com>
2017-11-30 00:40:43 +0800

    Remove Pods folder, should be created by pod install

--- Pods/Target Support Files/RxCocoa/RxCocoa-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_RxCocoa : NSObject
-@end
-@implementation PodsDummy_RxCocoa
-@end
--- Pods/Target Support Files/XYPingUtil/XYPingUtil-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_XYPingUtil : NSObject
-@end
-@implementation PodsDummy_XYPingUtil
-@end
