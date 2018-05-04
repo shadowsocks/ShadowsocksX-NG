ae5f16df2f24a8593b8ae2aff8cde146ab57c929
Qiu Yuzhou <qiuyuzhou@gmail.com>
2018-05-04 12:38:42 +0800

    Upgrade kcptun to v20170718.

--- ShadowsocksX-NG/LaunchAgentUtils.swift
+++ ShadowsocksX-NG/LaunchAgentUtils.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 let SS_LOCAL_VERSION = "3.1.3"
-let KCPTUN_CLIENT_VERSION = "20170322"
+let KCPTUN_CLIENT_VERSION = "v20170718"
 let PRIVOXY_VERSION = "3.0.26.static"
 let SIMPLE_OBFS_VERSION = "0.0.2"
 let APP_SUPPORT_DIR = "/Library/Application Support/ShadowsocksX-NG/"
--- ShadowsocksX-NG/install_kcptun.sh
+++ ShadowsocksX-NG/install_kcptun.sh
@@ -6,13 +6,19 @@
 #  Created by 邱宇舟 on 2017/1/11.
 #  Copyright © 2017年 qiuyuzhou. All rights reserved.
 
-VERSION="20170322"
+# https://github.com/shadowsocks/kcptun/releases
+
+VERSION="v20170718"
 
 cd `dirname "${BASH_SOURCE[0]}"`
 
 mkdir -p "$HOME/Library/Application Support/ShadowsocksX-NG/kcptun_$VERSION"
 cp -f kcptun_client "$HOME/Library/Application Support/ShadowsocksX-NG/kcptun_$VERSION/"
+
 rm -f "$HOME/Library/Application Support/ShadowsocksX-NG/kcptun_client"
 ln -s "$HOME/Library/Application Support/ShadowsocksX-NG/kcptun_$VERSION/kcptun_client" "$HOME/Library/Application Support/ShadowsocksX-NG/kcptun_client"
+
+rm -f "$HOME/Library/Application Support/ShadowsocksX-NG/plugins/kcptun"
+ln -s "$HOME/Library/Application Support/ShadowsocksX-NG/kcptun_$VERSION/kcptun_client" "$HOME/Library/Application Support/ShadowsocksX-NG/plugins/kcptun"
 
 echo "install kcptun done"
