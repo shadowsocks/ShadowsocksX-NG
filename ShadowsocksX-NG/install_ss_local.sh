edeaa5a78a7d10eae13829aefada80dda5404e3f
Qiu Yuzhou <qiuyuzhou@gmail.com>
2018-05-08 12:14:28 +0800

    Fixes project.

--- ShadowsocksX-NG/install_ss_local.sh
+++ ShadowsocksX-NG/install_ss_local.sh
@@ -24,19 +24,19 @@ cp -f libev.4.dylib "$TargetDir"
 
 # 2.8.0 https://bintray.com/homebrew/bottles/mbedtls
 cp -f libmbedcrypto.2.8.0.dylib "$TargetDir"
-ln -s  "$TargetDir/libmbedcrypto.2.8.0.dylib" "$TargetDir/libmbedcrypto.1.dylib"
+ln -sfh  "$TargetDir/libmbedcrypto.2.8.0.dylib" "$TargetDir/libmbedcrypto.1.dylib"
 
 # 8.42 https://bintray.com/homebrew/bottles/pcre
 cp -f libpcre.1.dylib "$TargetDir"
 
 # 1.0.16 https://bintray.com/homebrew/bottles/libsodium
 cp -f libsodium.23.dylib "$TargetDir"
-ln -s "$TargetDir/libsodium.23.dylib" "$TargetDir/libsodium.dylib"
+ln -sfh "$TargetDir/libsodium.23.dylib" "$TargetDir/libsodium.dylib"
 
 #cp -f libudns.0.dylib "$TargetDir"
 
 # 1.14.0 https://bintray.com/homebrew/bottles/c-ares
 cp -f libcares.2.dylib "$TargetDir"
-ln -s "$TargetDir/libcares.2.dylib" "$TargetDir/libcares.dylib"
+ln -sfh "$TargetDir/libcares.2.dylib" "$TargetDir/libcares.dylib"
 
 echo done
